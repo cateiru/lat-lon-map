7ec2840fb49e8675885b3e68eb88ed65a26b3da8
Yuto Watanabe <yuto.w51942@gmail.com>
2020-09-20 17:54:13 +0900

    yatta

--- src/main.ts
+++ src/main.ts
@@ -1,101 +1,29 @@
-/*!
- * @author: Yuto Watanabe
- * @version: 1.0.0
- *
- * Copyright (c) 2020 Earthquake alert
- */
-
-import "ol/ol.css";
 import Map from "ol/Map";
 import View from "ol/View";
-
-import * as control from "ol/control";
-import * as Format from "ol/format";
-import * as Layer from "ol/layer";
+import VectorTileLayer from "ol/layer/VectorTile";
+import VectorTileSource from "ol/source/VectorTile";
+import MVTFormat from "ol/format/MVT";
 import { fromLonLat } from "ol/proj";
-import * as Source from "ol/source";
 
-import { Style, Stroke, Fill } from "ol/style";
+import "ol/ol.css";
 
-// 地図データ
-const attribution = new control.Attribution({
-  className: "copyright",
-  collapsible: false,
-});
-
-// 都道府県のマップ
-const prefMap = new Layer.VectorTile({
-  source: new Source.VectorTile({
-    format: new Format.MVT(),
+// ベクトルタイルの olレイヤを定義
+const vt = new VectorTileLayer({
+  source: new VectorTileSource({
+    format: new MVTFormat(),
     url:
-      "https://earthquake-alert.github.io/maps/pbf_japan/pref_jma/{z}/{x}/{y}.pbf",
+      "https://cyberjapandata.gsi.go.jp/xyz/experimental_bvmap/{z}/{x}/{y}.pbf",
     attributions: [
-      "Copyright(c) 2020 Earthquake alert / 地図データ: 国土数値情報(湖沼データ), 気象庁(地震情報/細分区域, 緊急地震速報/府県予報区)",
+      'https://github.com/gsi-cyberjapan/gsimaps-vector-experiment" target="_blank" rel=”noopener noreferrer”>国土地理院',
     ],
   }),
-  style: new Style({
-    stroke: new Stroke({
-      color: "5b626e",
-      width: 3,
-    }),
-  }),
-  maxZoom: 10,
-  minZoom: 0,
-  maxResolution: 5000,
 });
 
-// 水域マップ
-const waterAreaMap = new Layer.VectorTile({
-  source: new Source.VectorTile({
-    format: new Format.MVT(),
-    url:
-      "https://earthquake-alert.github.io/maps/pbf_water_area/waterArea/{z}/{x}/{y}.pbf",
-  }),
-  style: new Style({
-    fill: new Fill({
-      color: "5b626e",
-    }),
-  }),
-  maxZoom: 10,
-  minZoom: 0,
-  maxResolution: 5000,
-});
-
-// 世界のマップ
-const worldMap = new Layer.VectorTile({
-  source: new Source.VectorTile({
-    format: new Format.MVT(),
-    url:
-      "https://earthquake-alert.github.io/maps/pbf_world/world/{z}/{x}/{y}.pbf",
-    attributions: [
-      "Copyright(c) 2020 Earthquake alert / 地図データ: Natural Earth",
-    ],
-  }),
-  style: new Style({
-    fill: new Fill({
-      color: "5b626e",
-    }),
-    stroke: new Stroke({
-      color: "5b626e",
-      width: 2,
-    }),
-  }),
-  maxZoom: 5,
-  minZoom: 0,
-  minResolution: 5000,
-});
-
-// マップ描画
-const map = new Map({
+new Map({
   target: "map",
   view: new View({
-    center: fromLonLat([35.362222, 138.731388]),
-    zoom: 5,
-    maxZoom: 10,
+    center: fromLonLat([140.46, 36.37]),
+    zoom: 10,
   }),
-  layers: [
-    worldMap,
-    waterAreaMap,
-    prefMap,
-  ],
+  layers: [vt],
 });
